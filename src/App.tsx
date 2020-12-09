c43a6d3269d4df46cd14dcac189dd9d3a8d93c34
won <rjsdnql123@gmail.com>
2020-12-09 17:49:20 +0900

    Add Logo hover event

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Logo from "./Logo";
 
 function App() {
+  axios
+    .get(
+      "/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?ServiceKey=87YrGK0UyFcOLZaxfCt%2BHGjiD%2F8YEe%2FqteSCkhcWh%2F3OXV4XpJtZUwFsnseKBSRc5etkQw6JtzeN86qGQQfFVQ%3D%3D&LAWD_CD=11110&DEAL_YMD=201512"
+    )
+    .then((Response) => {
+      console.log(Response);
+    })
+    .catch((Error) => {
+      console.log(Error);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo></Logo>
     </div>
   );
 }
